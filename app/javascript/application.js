f1ddbf262511fbdee974a8389085e0c2d0a7b69d
Danny Jenuil <45913874+danjenuil@users.noreply.github.com>
2023-08-10 23:04:06 +0800

    Tidy up form styling and add JavaScript to disable form components upon submission.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "popper"
 import "bootstrap"
+
+document.addEventListener("turbo:submit-start", ({ target }) => {
+    for (const field of target.elements) {
+        field.disabled = true
+    }
+})
